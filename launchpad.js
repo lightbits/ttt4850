04deb1e858e695a8f3b03e3c45da17bc9e880cf3
lightbits <simenh.jobb@gmail.com>
2016-02-24 11:44:36 +0100

    Created button sound array

--- launchpad.js
+++ launchpad.js
@@ -3,6 +3,43 @@
 // Approach 1: Alternate columns with "raw" sample and "melody" samples.
 // Approach 2: Sound does not change across columns. Have some melody samples.
 
+// The Launchpad button layout
+// is represented as a 2D array
+// of 8x8 buttons mapped to the
+// following MIDI button indices.
+//                    [USB]
+//                      |
+//     +------------------+------+
+//     | 104 ... ... ... ... 111 |
+//     +-------------------------+
+//   7 | 81 82 83 84 85 86 87 88 | Volume:     89
+//   6 | 71 72 73 74 75 76 77 78 | Pan:        79
+//   5 | 61 62 63 64 65 66 67 68 | Send A:     69
+// Y 4 | 51 52 53 54 55 56 57 58 | Send B:     59
+//   3 | 41 42 43 44 45 46 47 48 | Stop:       49
+//   2 | 31 32 33 34 35 36 37 38 | Mute:       39
+//   1 | 21 22 23 24 25 26 27 28 | Solo:       29
+//   0 | 11 12 13 14 15 16 17 18 | Record Arm: 19
+//     +-------------------------+
+//        0  1  2  3  4  5  6  7
+//                  X
+
+function XYToMidiIndex(x, y)
+{
+    if (x < 0) x = 0;
+    if (x > 7) x = 7;
+    if (y < 0) y = 0;
+    if (y > 7) y = 7;
+    return 11 + y*10 + x;
+}
+
+function MidiIndexToXY(index)
+{
+    x = (index-11)%10;
+    y = (index-11)/10;
+    return [x, y];
+}
+
 function main()
 {
     var MIDI = null;
@@ -21,43 +58,6 @@ function main()
     var SAMPLE6 = {buffer: null};
     var SAMPLE7 = {buffer: null};
 
-    // The Launchpad button layout
-    // is represented as a 2D array
-    // of 8x8 buttons mapped to the
-    // following MIDI button indices.
-    //                    [USB]
-    //                      |
-    //     +------------------+------+
-    //     | 104 ... ... ... ... 111 |
-    //     +-------------------------+
-    //   7 | 81 82 83 84 85 86 87 88 | Volume:     89
-    //   6 | 71 72 73 74 75 76 77 78 | Pan:        79
-    //   5 | 61 62 63 64 65 66 67 68 | Send A:     69
-    // Y 4 | 51 52 53 54 55 56 57 58 | Send B:     59
-    //   3 | 41 42 43 44 45 46 47 48 | Stop:       49
-    //   2 | 31 32 33 34 35 36 37 38 | Mute:       39
-    //   1 | 21 22 23 24 25 26 27 28 | Solo:       29
-    //   0 | 11 12 13 14 15 16 17 18 | Record Arm: 19
-    //     +-------------------------+
-    //        0  1  2  3  4  5  6  7
-    //                  X
-
-    function XYToMidiIndex(x, y)
-    {
-        if (x < 0) x = 0;
-        if (x > 7) x = 7;
-        if (y < 0) y = 0;
-        if (y > 7) y = 7;
-        return 11 + y*10 + x;
-    }
-
-    function MidiIndexToXY(index)
-    {
-        x = (index-11)%10;
-        y = (index-11)/10;
-        return [x, y];
-    }
-
     var AudioContext = AudioContext || webkitAudioContext; // for ios/safari
     var CONTEXT = new AudioContext();
 
@@ -70,53 +70,94 @@ function main()
     LoadSample(SAMPLE6, "assets/B/flash-2.mp3");
     LoadSample(SAMPLE7, "assets/B/flash-3.mp3");
 
+    var BUTTONS = [];
+    for (var x = 0; x < 8; x++)
+    {
+        BUTTONS[XYToMidiIndex(x, 0)] = {sample: SAMPLE0, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 1)] = {sample: SAMPLE1, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 2)] = {sample: SAMPLE2, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 3)] = {sample: SAMPLE3, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 4)] = {sample: SAMPLE4, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 5)] = {sample: SAMPLE5, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 6)] = {sample: SAMPLE6, is_set: false};
+        BUTTONS[XYToMidiIndex(x, 7)] = {sample: SAMPLE7, is_set: false};
+    }
+
+    function PlayButton(x, y)
+    {
+        if (x >= 0 && x <= 7 && y >= 0 && y <= 7)
+        {
+            button = BUTTONS[XYToMidiIndex(x, y)];
+            if (button.is_set)
+                PlaySample(button.sample, 1.0, 1.0);
+        }
+        else
+        {
+            console.log("@PlayButton: Indices out of range");
+        }
+    }
+
     function OnMidiMessage(event)
     {
         console.log(event);
-        var button = event.data[1];
+        var button_index = event.data[1];
         var down = event.data[2] == 0;
+
+        if (down)
+        {
+            if (BUTTONS[button_index].is_set)
+            {
+                BUTTONS[button_index].is_set = false;
+                NoteColor(button_index, 0);
+            }
+            else
+            {
+                BUTTONS[button_index].is_set = true;
+                NoteColor(button_index, 35);
+            }
+        }
 
         // Simple example functionality:
         // SESSION button turns off all lights
         // UP arrow increases next color value
         // DOWN arrow decreases next color value
         // Pushing a button sets its light and plays a sound
-        if (down && button == 104)
-        {
-            if (COLOR < 128) COLOR++;
-        }
-        else if (down && button == 105)
-        {
-            if (COLOR > 0) COLOR--;
-        }
-        else if (down && button == 108)
-        {
-            for (var button = 0; button < 128; button++)
-            {
-                NoteColor(button, 0);
-            }
-        }
-        else if (down)
-        {
-            if (button == 71)
-                PlaySample(SAMPLE0, 1.0, 1.0);
-            else if (button == 72)
-                PlaySample(SAMPLE1, 1.0, 1.0);
-            else if (button == 73)
-                PlaySample(SAMPLE2, 1.0, 1.0);
-            else if (button == 74)
-                PlaySample(SAMPLE3, 1.0, 1.0);
-            else if (button == 81)
-                PlaySample(SAMPLE4, 1.0, 1.0);
-            else if (button == 82)
-                PlaySample(SAMPLE5, 1.0, 1.0);
-            else if (button == 83)
-                PlaySample(SAMPLE6, 1.0, 1.0);
-            else if (button == 84)
-                PlaySample(SAMPLE7, 1.0, 1.0);
-            console.log("Setting color " + COLOR);
-            NoteColor(button, COLOR);
-        }
+        // if (down && button == 104)
+        // {
+        //     if (COLOR < 128) COLOR++;
+        // }
+        // else if (down && button == 105)
+        // {
+        //     if (COLOR > 0) COLOR--;
+        // }
+        // else if (down && button == 108)
+        // {
+        //     for (var button = 0; button < 128; button++)
+        //     {
+        //         NoteColor(button, 0);
+        //     }
+        // }
+        // else if (down)
+        // {
+        //     if (button == 71)
+        //         PlaySample(SAMPLE0, 1.0, 1.0);
+        //     else if (button == 72)
+        //         PlaySample(SAMPLE1, 1.0, 1.0);
+        //     else if (button == 73)
+        //         PlaySample(SAMPLE2, 1.0, 1.0);
+        //     else if (button == 74)
+        //         PlaySample(SAMPLE3, 1.0, 1.0);
+        //     else if (button == 81)
+        //         PlaySample(SAMPLE4, 1.0, 1.0);
+        //     else if (button == 82)
+        //         PlaySample(SAMPLE5, 1.0, 1.0);
+        //     else if (button == 83)
+        //         PlaySample(SAMPLE6, 1.0, 1.0);
+        //     else if (button == 84)
+        //         PlaySample(SAMPLE7, 1.0, 1.0);
+        //     console.log("Setting color " + COLOR);
+        //     NoteColor(button, COLOR);
+        // }
     }
 
     function SendNoteMessage(data)
@@ -224,12 +265,12 @@ function main()
         STATUS.innerHTML = "Failed to get MIDI access - " + msg;
     }
 
-    function MusicLoop(interval) 
+    function MusicLoop(interval)
     {
         window.setInterval(PlayColumn(), interval);
     }
 
-    function PlayColumn() 
+    function PlayColumn()
     {
         samples = getSampleColumn(COLUMN);
         for (var i = 0; i < samples.length; i++) {
