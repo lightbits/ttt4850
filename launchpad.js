8f28b6243b1bd1ea6dbb1fba1e39392418593385
lightbits <simenh.jobb@gmail.com>
2016-02-24 11:03:18 +0100

    Added XY and Index conversions

--- launchpad.js
+++ launchpad.js
@@ -20,17 +20,54 @@ function main()
     var SAMPLE6 = {buffer: null};
     var SAMPLE7 = {buffer: null};
 
+    // The Launchpad button layout
+    // is represented as a 2D array
+    // of 8x8 buttons mapped to the
+    // following MIDI button indices.
+    //                    [USB]
+    //                      |
+    //     +------------------+------+
+    //     | 104 ... ... ... ... 111 |
+    //     +-------------------------+
+    //   7 | 81 82 83 84 85 86 87 88 | Volume:     89
+    //   6 | 71 72 73 74 75 76 77 78 | Pan:        79
+    //   5 | 61 62 63 64 65 66 67 68 | Send A:     69
+    // Y 4 | 51 52 53 54 55 56 57 58 | Send B:     59
+    //   3 | 41 42 43 44 45 46 47 48 | Stop:       49
+    //   2 | 31 32 33 34 35 36 37 38 | Mute:       39
+    //   1 | 21 22 23 24 25 26 27 28 | Solo:       29
+    //   0 | 11 12 13 14 15 16 17 18 | Record Arm: 19
+    //     +-------------------------+
+    //        0  1  2  3  4  5  6  7
+    //                  X
+
+    function XYToMidiIndex(x, y)
+    {
+        if (x < 0) x = 0;
+        if (x > 7) x = 7;
+        if (y < 0) y = 0;
+        if (y > 7) y = 7;
+        return 11 + y*10 + x;
+    }
+
+    function MidiIndexToXY(index)
+    {
+        x = (index-11)%10;
+        y = (index-11)/10;
+        return [x, y];
+    }
+
     var AudioContext = AudioContext || webkitAudioContext; // for ios/safari
     var CONTEXT = new AudioContext();
 
-    LoadSample(CONTEXT, SAMPLE0, "assets/B/bubbles.mp3");
-    LoadSample(CONTEXT, SAMPLE1, "assets/B/clay.mp3");
-    LoadSample(CONTEXT, SAMPLE2, "assets/B/confetti.mp3");
-    LoadSample(CONTEXT, SAMPLE3, "assets/B/corona.mp3");
-    LoadSample(CONTEXT, SAMPLE4, "assets/B/dotted-spiral.mp3");
-    LoadSample(CONTEXT, SAMPLE5, "assets/B/flash-1.mp3");
-    LoadSample(CONTEXT, SAMPLE6, "assets/B/flash-2.mp3");
-    LoadSample(CONTEXT, SAMPLE7, "assets/B/flash-3.mp3");
+    LoadSample(SAMPLE0, "assets/B/bubbles.mp3");
+    LoadSample(SAMPLE1, "assets/B/clay.mp3");
+    LoadSample(SAMPLE2, "assets/B/confetti.mp3");
+    LoadSample(SAMPLE3, "assets/B/corona.mp3");
+    LoadSample(SAMPLE4, "assets/B/dotted-spiral.mp3");
+    LoadSample(SAMPLE5, "assets/B/flash-1.mp3");
+    LoadSample(SAMPLE6, "assets/B/flash-2.mp3");
+    LoadSample(SAMPLE7, "assets/B/flash-3.mp3");
 
     function OnMidiMessage(event)
     {
@@ -134,32 +171,32 @@ function main()
         }
     }
 
-    function PlaySample(context, sample, gain, rate)
+    function PlaySample(sample, gain, rate)
     {
-        var s = context.createBufferSource();
-        var g = context.createGain();
+        var s = CONTEXT.createBufferSource();
+        var g = CONTEXT.createGain();
         s.buffer = sample.buffer;
         s.playbackRate.value = rate;
         s.connect(g);
         g.gain.value = gain;
-        g.connect(context.destination);
+        g.connect(CONTEXT.destination);
         s.start();
         sample.s = s;
     }
 
-    function StopSample(context, sample)
+    function StopSample(sample)
     {
         sample.s.stop();
     }
 
-    function LoadSample(context, sample, url)
+    function LoadSample(sample, url)
     {
-        console.log(context);
+        console.log(CONTEXT);
         var request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.responseType = 'arraybuffer';
         request.onload = function() {
-            context.decodeAudioData(request.response, function(buffer) {
+            CONTEXT.decodeAudioData(request.response, function(buffer) {
                 sample.buffer = buffer;
             });
         }
